f9f0b918f008494c38fbe27a2c261bd47ff3797c
islamrifatislam <islamrifatislam500@gmail.com>
2025-07-23 22:48:00 +0600

    added all section pracctice done

---
+++ math.js
@@ -0,0 +1,6 @@
+const today=new Date('2026-10-09');
+console.log(today);
+console.log(today.getMonth())
+console.log(today.getDay())
+// const  today =new date ('2062-10-09')
+// console.log(today);
